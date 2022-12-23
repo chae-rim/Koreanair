7156533fc6f3ff176f467f47402bb44afa8eee13
chae-rim <108618066+chae-rim@users.noreply.github.com>
2022-12-23 09:25:38 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+//네비
+$(document).ready(function () {
+    $('#header .nav_bar').hide();
+    $('.nav_group li').click(function () {
+        $('#header .nav_bar').toggle();
+    });
+})
+
+$(document).ready(function () {
+    $('#header .nav_bar').hide();
+    $('.close button').click(function () {
+        $('#header .nav_bar').hide();
+    });
+})
+
+
+
+
+
+
+//메인이미지
+$(document).ready(function () {
+    var current = 0
+    var setIntervalId;
+
+    timer();
+    function timer() {
+        setIntervalId = setInterval(function () {
+            var n = current + 1;
+            if (n == 4) {
+                n = 0;
+            }
+            move(n);
+        }, 5000)
+    }
+    function move(n) {
+        var currentEl = $('.main_img li').eq(current);
+        var nextEl = $('.main_img li').eq(n);
+
+        currentEl.css({ left: '0%' }).animate({ left: '-100%' });
+        nextEl.css({ left: '100%' }).animate({ left: '0%' });
+
+        current = n;
+    }
+});
